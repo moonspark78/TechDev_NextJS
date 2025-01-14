43e2ceb2cc75e6634302b2d674a8637c1150217e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-14 04:00:17 +0100

    working on  about Section

--- app/components/Home/About/About.tsx
+++ app/components/Home/About/About.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const About = () => {
   return (
@@ -19,6 +20,35 @@ const About = () => {
               <h1 className='border-2 px-4 w-fit rounded-lg bg-gray-100 py-1 text-[#f68967] font-bold'>
                 About Us
               </h1>
+              <h1 className='text-2xl sm:text-3xl font-bold text-gray-900 mt-3 leading-[2.2rem] sm:leading-[2.2rem]'>
+                Everything You Need To Grow Your Business
+              </h1>
+              <p className='mt-3 leading-relaxed text-sm sm:text-base text-gray-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae vitae et ratione, deleniti inventore aperiam
+                ut qui totam illo necessitatibus esse, quo voluptas, sit nobis quia deserunt aliquid. Rerum voluptate eius magni,
+                eligendi nisi iure perspiciatis ab similique quibusdam suscipit vitae ducimus magnam non nobis.
+              </p>
+              <button className='mt-5 text-[#f68967] font-bold pb-1 border-b-2 border-[#f68967] flex items-center'>
+                Learn More
+                <FaArrowRightLong size={20} className='ml-3'/>
+              </button>
+              <div className='mt-6 border-l-2 border-gray-200'>
+                <div className='ml-6'>
+                  <p className='text-gray-700 font-medium'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi explicabo, 
+                    cupiditate nihil odit magnam aliquam ad reprehenderit delectus saepe assumenda.
+                  </p>
+                  <div>
+                    <Image
+                      src="/images/u1.jpg"
+                      alt='user'
+                      width={40}
+                      height={40}
+                      className='rounded-full'
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
     </div>
