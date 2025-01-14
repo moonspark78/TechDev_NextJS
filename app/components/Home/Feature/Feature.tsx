f40b92c5c33af3ddb323982b83a64feb407ba3b4
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-14 04:51:51 +0100

    working on  Feature Components

--- app/components/Home/Feature/Feature.tsx
+++ app/components/Home/Feature/Feature.tsx
@@ -7,6 +7,17 @@ const Feature = () => {
             <h1 className='text-center text-2xl text-blue-950 font-bold'>
                 Key Features Of The Products
             </h1>
+            <p className='mt-3 text-center font-medium text-gray-700 w-[90%] mx-auto sm:w-[70%] md:w-[50%]'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus tempore possimus eos optio reiciendis quo.
+            </p>
+        </div>
+
+        {/* Main Grid */}
+        <div className='w-[80%] mx-auto mt-16 grid grid-cols-1 xl:grid-cols-2 gap-10 items-center'>
+            {/* Inner Grid */}
+            <div className=''>
+
+            </div>
         </div>
     </div>
   )
