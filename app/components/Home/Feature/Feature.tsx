6abef70e2855cc8916e8dc5b377357de57b24974
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-14 04:47:28 +0100

    working on  Feature components

--- app/components/Home/Feature/Feature.tsx
+++ app/components/Home/Feature/Feature.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <div className='pt-16 pb-16 bg-gray-100'>
+        <div>
+            <h1 className='text-center text-2xl text-blue-950 font-bold'>
+                Key Features Of The Products
+            </h1>
+        </div>
+    </div>
   )
 }
 
