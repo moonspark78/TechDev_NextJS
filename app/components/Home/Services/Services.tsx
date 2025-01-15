217beae7bc4972d40cb552ae0bdf844de0aafebb
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-15 02:12:43 +0100

    create services components

--- app/components/Home/Services/Services.tsx
+++ app/components/Home/Services/Services.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='pt-16 pb-16'>
+      <div className='w-[80%] mx-auto'>
+        <h1 className='border-2 px-4 w-fit rounded-lg bg-gray-100 py-1 text-[#f68967] font-bold'>
+          Services
+        </h1>
+        {/* Heading */}
+        <h1 className='mt-4 text-2xl md:text-3xl font-bold text-gray-800'>
+          Our Services Made For You?
+        </h1>
+        {/* Description */}
+        <p className='mt-4 w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] text-gray-500'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus culpa repudiandae
+          architecto temporibus ut itaque assumenda voluptatem enim accusantium voluptatum.
+        </p>
+      </div>
+    </div>
   )
 }
 
