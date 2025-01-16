b87f415fdddb768cba5ec30e66a01daa1fc60ae5
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-16 09:30:01 +0100

    working  Team component

--- app/components/Home/Team/Team.tsx
+++ app/components/Home/Team/Team.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div className="pt-16 pb-16">
+      <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-5 gap-10 items-center">
+        {/* Text Content */}
+        <div className="lg:col-span-2">
+          <h1 className="border-2 px-4 w-fit rounded-lg bg-gray-100 py-1 text-[#f68967] font-bold">
+            Our Teams
+          </h1>
+          {/* Heading */}
+        </div>
+        {/* Image Content */}
+        <div className="lg:col-span-3">
+            <Image src="" alt="" width={200} height={200} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Team
+export default Team;
