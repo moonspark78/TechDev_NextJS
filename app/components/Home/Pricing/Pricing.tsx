470f3a149be832470976e718aa6874ffff064bcd
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-16 11:08:17 +0100

    Create Pricing Component & PricingCard

--- app/components/Home/Pricing/Pricing.tsx
+++ app/components/Home/Pricing/Pricing.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <div className='pt-16 pb-16 bg-gray-100'>
+        <h1 className='text-center text-2xl text-blue-950 font-bold'>
+
+        </h1>
+        <p className='mt-3 text-center font-medium text-gray-700 w-[90%] sm:w-[70%] md:w-[50%] mx-auto'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquam rem facilis quae nulla voluptatem.
+        </p>
+        <div className='w-[80%] mx-auto mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            <div>
+                {/* PricingCard */}
+            </div>
+        </div>
+    </div>
   )
 }
 
