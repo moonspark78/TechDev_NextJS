4999eba10b81d949987abe499174166b3287dbd0
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-17 16:57:10 +0100

    working  a Footer Component

--- app/components/Home/Footer/Footer.tsx
+++ app/components/Home/Footer/Footer.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='pt-16 pb-16'>
+      <div className='w-[80%] mx-auto items-start grid-cols-1 sm:grid-cols-2 grid md:grid-cols-3 lg:grid-cols-4'>
+        {/* 1st Part */}
+          <div className='space-y-5'>
+            <h1 className='text-lg font-bold'>Solution</h1>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>
+              Enterprice
+            </p>
+          </div>
+      </div>
+    </div>
   )
 }
 
